7af70744199b113bb27167252dae4232d321e744
Mirnesa Hodzic <mirnesa.hodzic@kindredgroup.cz>
2018-11-24 15:47:56 +0100

    https://github.com/HackerSchoolCZ/hackathon_team_1

--- Image_Recognision/Resources/images/test_data_mahara.py
+++
@@ -1,9 +0,0 @@
-def get_variables():
-    variables = {"INVALID_LOGIN": "pavel",
-                 "INVALID_PASSWORD": "aa",
-                 "VALID_LOGIN": "admin",
-                 "VALID_PASSWORD": "MaharaDemo",
-                 "TEST_DATA_FILE_INVALID": "C:\\Users\\PSaman\\Desktop\\python\\robot\\hs\\Resources\\mahara\\test_data_data_driven_approach_invalid.csv",
-                 "TEST_DATA_FILE_VALID": "C:\\Users\\PSaman\\Desktop\\python\\robot\\hs\\Resources\\mahara\\test_data_data_driven_approach_valid.csv"
-                 }
-    return variables
